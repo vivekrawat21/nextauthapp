5653a43473f4ee828117c80de50ac7bbe69a6cf0
vivekrawat21 <vivekrwt2111@gmail.com>
2024-01-24 15:37:08 +0530

    some errors

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -8,7 +8,8 @@ import Link from "next/link";
 export default function ProfilePage() {
     
     const [loading, setLoading] = React.useState(false)
-    const[data,setData]:any = React.useState({});
+    const[data,setData]:any = React.useState("nothing");
+    const [name ,setName]:any = React.useState("");
     const router = useRouter();
     const onLogout = async () => {
         try {
@@ -34,13 +35,14 @@ export default function ProfilePage() {
     const getUserDetails = async () => {
        const response =  await axios.get("/api/users/me");
        console.log(response.data);
-        setData(response.data.data);
+        setData(response.data.data._id);
+        setName(response.data.data.username);
     }
     return (
         <div className="flex flex-col items-center justify-center min-h-screen">
             <h1 className="text-cyan-400 text-2xl font-extrabold">UserProfile</h1>
             <hr />
-            <p className="text-xl font-serif text-purple-100">Profile page:  {Object.entries(data).length===0?"click on button to get details":<Link href={`/profile/${data._id}`}><span className="underline text-blue-500 font-mono text-lg">{data.username}</span></Link>}</p>
+            <p className="text-xl font-serif text-purple-100">Profile page:  {data ==='nothing'?"click on button to get details":<Link href={`/profile/${data}`}><span className="underline text-blue-500 font-mono text-lg">{name}</span></Link>}</p>
             <hr />
 
             <button className="p-5 my-6 border   rounded-lg border-red-300 hover:bg-gray-900" onClick={onLogout} >
