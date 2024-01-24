9aa90bead302f5513085785106ed88081d2cb192
vivekrawat21 <vivekrwt2111@gmail.com>
2024-01-24 16:02:00 +0530

    error

--- src/app/api/users/me/route.ts
+++ src/app/api/users/me/route.ts
@@ -8,8 +8,8 @@ connect();
 
 export async function GET(request: NextRequest){
     try {
-       const userId =  await getDataFromToken(request);
-      const user = await User.findById(userId).select("-password -isAdmin");
+     const userId =  await getDataFromToken(request);
+      const user = await User.findById({_id:userId}).select("-password -isAdmin");
       return NextResponse.json({
         message:"User found ",
         data: user
@@ -17,7 +17,7 @@ export async function GET(request: NextRequest){
     } catch (error:any) {
         return NextResponse.json({
             error: error.message
-        })
+        },{status:400})
         
     }
 }
