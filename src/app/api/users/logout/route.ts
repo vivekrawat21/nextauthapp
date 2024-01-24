607d08897dce4745ba46e231e428539ffb7db87d
vivekrawat21 <vivekrwt2111@gmail.com>
2024-01-24 16:38:31 +0530

    errror

--- src/app/api/users/logout/route.ts
+++ src/app/api/users/logout/route.ts
@@ -7,7 +7,7 @@ export async function GET(){
     success: true,
    });
    response.cookies.set("token" , "",
-   {httpOnly: true , expires: new Date(0)
+   { httpOnly: true , expires: new Date(0)
 });
 
    return response;
