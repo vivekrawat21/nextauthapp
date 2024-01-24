5842109508e55e0b253860024d27c0858e8a4b17
vivekrawat21 <vivekrwt2111@gmail.com>
2024-01-24 16:37:49 +0530

    error

--- src/app/api/users/logout/route.ts
+++ src/app/api/users/logout/route.ts
@@ -1,19 +1,16 @@
-import { connect } from "@/dbConfig/dbConfig";
-
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
 export async function GET(){
     try{
-   const respose = NextResponse.json({
-    status: 200,
+   const response = NextResponse.json({
     message :"user logout successfully",
     success: true,
    });
-   respose.cookies.set("token","",
+   response.cookies.set("token" , "",
    {httpOnly: true , expires: new Date(0)
 });
 
-   return respose;
+   return response;
     }
     catch(error: any){
         return NextResponse.json({error:error.message},{status:500});
